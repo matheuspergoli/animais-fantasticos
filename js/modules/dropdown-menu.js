73a6f1b77622ac7082634794b2be14a5e2b87c81
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-04-26 18:44:27 -0300

    Dropdown update

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,15 +1,16 @@
 export default function initDropdownMenu() {
-  const dropdownMenus = document.querySelectorAll('[data-dropdown]')
-  const userEvents = ['touchstart', 'click']
+  
+}
 
-  dropdownMenus.forEach(menu => {
-    userEvents.forEach(userEvent => {
-      menu.addEventListener(userEvent, handleClick)
-    })
+const dropdownMenus = document.querySelectorAll('[data-dropdown]')
+
+dropdownMenus.forEach(menu => {
+  ['touchstart', 'click'].forEach(userEvent => {
+    menu.addEventListener(userEvent, handleClick)
   })
+})
 
-  function handleClick(event) {
-    event.preventDefault()
-    this.classList.toggle('active')
-  }
+function handleClick(event) {
+  event.preventDefault()
+  this.classList.toggle('active')
 }
