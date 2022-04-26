8a6670465cb9732171f63f7cb76b91a317f4bc65
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-04-26 18:08:36 -0300

    Dropdown menu

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAnimacaoScroll from "./modules/scroll-animacao.js"
 import initAccordion from "./modules/accordion.js"
 import initTabNav from "./modules/tabnav.js"
 import initModal from "./modules/modal.js"
+import initDropdownMenu from "./modules/dropdown-menu.js"
 
 initScrollSuave()
 initAnimacaoScroll()
 initAccordion()
 initTabNav()
 initModal()
+initDropdownMenu()
